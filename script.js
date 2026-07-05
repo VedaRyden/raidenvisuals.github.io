18a09bc366c57043b4f0a744dfe4af32d69b1c9f
VEDA_RAIDEN <BUSSINESVEDA@GMAIL.COM>
2026-07-05 17:59:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// 1. FITUR PENCARIAN (LIVE SEARCH)
-const searchInput = document.getElementById('searchInput');
-
-searchInput.addEventListener('keyup', function() {
-    const filterText = searchInput.value.toLowerCase();
-    const cards = document.querySelectorAll('.download-card');
-
-    cards.forEach(card => {
-        const fileName = card.querySelector('.file-name').textContent.toLowerCase();
-        
-        // Jika nama file cocok dengan ketikan, tampilkan. Jika tidak, sembunyikan.
-        if (fileName.includes(filterText)) {
-            card.style.display = "flex";
-        } else {
-            card.style.display = "none";
-        }
-    });
-});
-
 // 2. FITUR POP-UP NOTIFIKASI SAAT KLIK DOWNLOAD
 const downloadButtons = document.querySelectorAll('.download-btn');
 
